f7b82f8e213cb7e28e6b0d03c6aa1494151bb8ae
customx <t.lucky7@icloud.com>
2024-09-02 07:02:17 +0900

    新しいbookデータを登録

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2024_09_01_212647) do
+
+  create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
+    t.integer "inout"
+    t.string "category"
+    t.integer "year"
+    t.integer "month"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
