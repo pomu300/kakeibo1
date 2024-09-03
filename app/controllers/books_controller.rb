bcc750178bbb629d7ed44d565c8d71d8a86fbf7b
customx <t.lucky7@icloud.com>
2024-09-03 10:46:30 +0900

    途中経過

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,15 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+    @book.year = 2019
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/books", to: "books#index"
+  get "/books/new", to: "books#new", as: "new_book"
+  get "/books/:id", to: "books#show", as: "book"
 end
