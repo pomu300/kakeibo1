c495d5bbd8c96c95ff2e1b3b8d196a6e36342068
customx <t.lucky7@icloud.com>
2024-09-04 16:20:15 +0900

    完成版（仮）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,56 @@
 class BooksController < ApplicationController
 
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
   def index
     @books = Book.all
+    @books = @books.where(year: params[:year]) if params[:year].present?
+    @books = @books.where(month: params[:month]) if params[:month].present?
   end
 
   def show
-    @book = Book.find(params[:id])
   end
 
   def new
     @book = Book.new
     @book.year = 2019
+  end
+
+  def create
+    book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "家計簿にデータを１件登録しました"
+      redirect_to books_path
+    else
+      flash.now[:alert] = "登録に失敗しました。"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
+    if @book.update(book_params)
+      flash[:notice] = "データを１件更新しました"
+      redirect_to books_path
+    else
+      flash.now[:alert] = "更新に失敗しました。"
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.destroy
+    flash[:notice] = "削除しました"
+    redirect_to books_path
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
