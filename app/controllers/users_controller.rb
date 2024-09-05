75828b142a067af9dbe9d2fcd673c2d79ebbf17e
customx <t.lucky7@icloud.com>
2024-09-06 07:09:26 +0900

    登録処理実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "登録しました"
+      # どこかへリダイレクト
+    else
+      flash.now[:alert] = "登録に失敗しました"
+      render "new"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
 
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
+  get "/signin", to: "session#new"
+  post "/signin", to: "session#create"
 end
